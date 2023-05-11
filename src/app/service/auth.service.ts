3c2a303535ca5f70fcd0aa632a14e8881999f25a
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-05-11 06:53:48 -0300

    fix url 2

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,10 +15,10 @@ export class AuthService {
   constructor(private httpClient: HttpClient) { }
 
  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
-   return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
+   return this.httpClient.post<any>(this.URL + 'nuevo', nuevoUsuario);
  }
 
  public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
-   return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUsuario)
+   return this.httpClient.post<JwtDto>(this.URL + 'login', loginUsuario)
  }
 }
