f698afff3e0ec179f0254ad3d05cc8b025aaa8e8
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-06-13 18:04:20 -0300

    update

--- src/app/components/pedidos/nuevo-pedido.component.ts
+++ src/app/components/pedidos/nuevo-pedido.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pedidos } from 'src/app/model/pedidos';
+import { PedidosService } from 'src/app/service/pedidos.service';
 
 @Component({
   selector: 'app-nuevo-pedido',
   templateUrl: './nuevo-pedido.component.html',
   styleUrls: ['./nuevo-pedido.component.css']
 })
-export class NuevoPedidoComponent {
+export class NuevoPedidoComponent implements OnInit {
+  numeroPedido: string;
+  numeroMesaPedido: string;
+  nombreClientePedido: string;
+  horarioPedido: string;
+  pedido: string;
+  observacionesPedido: string;
+  estadoPedido: string;
+  precioPedido: string;
+
+  //alerts
+  successMessage: string;
+  errorMessage: string;
+
+  constructor(private pedidosS: PedidosService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onCreate(): void{
+    const pedidos = new Pedidos(this.numeroPedido, this.numeroMesaPedido, this.nombreClientePedido, this.horarioPedido, this.pedido, this.observacionesPedido, this.estadoPedido, this.precioPedido);
+    this.pedidosS.save(pedidos).subscribe(
+      data =>{
+        this.successMessage = "Añadiste un nuevo pedido, redirigiendo...";
+        setTimeout(() => {
+          this.router.navigate(['']);
+        }, 2500);
+      }, err =>{
+        this.errorMessage = "Error al añadir el pedido.";
+        setTimeout(() => {
+          this.router.navigate(['']);
+        }, 2500);
+      }
+    )
+  }
 
 }
