37ed78f4eb94a7da146383e61bd33849254ea071
pabloquirogajuarez <pabloquirogajuarez@gmail.com>
2023-10-10 01:27:35 -0300

    se agrego la hora

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,7 +7,21 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
   fechaActual: Date;
+  horaActual: string;
+
   constructor() {
     this.fechaActual = new Date();
+    this.obtenerHoraActual();
+  }
+
+  obtenerHoraActual() {
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: 'America/Argentina/Buenos_Aires',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false // Para mostrar la hora en formato de 24 horas
+    };
+
+    this.horaActual = new Intl.DateTimeFormat('es-AR', options).format(this.fechaActual);
   }
 }
