5ab494f1fb743f098d1bd81014aaa9929a7a0680
pabloquirogajuarez <pabloquirogajuarez@gmail.com>
2023-12-07 18:50:35 -0300

    fix

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,26 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { TokenService } from 'src/app/service/token.service'; // Asegúrate de importar tu servicio de tokens
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   fechaActual: Date;
   horaActual: string;
 
-  constructor(private tokenService: TokenService, private router: Router) {
+  constructor() {
     this.fechaActual = new Date();
     this.obtenerHoraActual();
-  }
-
-  ngOnInit(): void {
-    const isAdmin = this.tokenService.hasRole('ROLE_ADMIN');
-    if (!isAdmin) {
-      this.router.navigate(['/']); 
-    }
   }
 
   obtenerHoraActual() {
